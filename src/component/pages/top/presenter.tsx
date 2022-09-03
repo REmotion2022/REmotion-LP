b7d532666370a3d20e3f5774b42ea1d1a659aff2
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-09-03 16:26:47 +0900

    usecase の UI 作成

--- src/component/pages/top/presenter.tsx
+++ src/component/pages/top/presenter.tsx
@@ -3,7 +3,10 @@ import styles from './style.module.scss'
 import { ProblemArea } from '~/component/uiParts/ProblemArea'
 import { SolutionArea } from '~/component/uiParts/SolutionArea'
 import { FeatureArea } from '~/component/uiParts/FeatureArea'
-// import Image from 'next/image'
+import Image from 'next/image'
+import SeminarImg from 'public/seminar.svg'
+import MeetingImg from 'public/meeting.svg'
+import ClassImg from 'public/class.svg'
 
 export const TopPagePresenter: React.FC = () => {
   return (
@@ -18,19 +21,28 @@ export const TopPagePresenter: React.FC = () => {
       <FeatureArea />
       <div className={styles.useCase}>
         <h1>REmotion の使用例</h1>
+        <p>REmotion はオンライン上で行う活動中のコミュニケーションをサポートします．</p>
         <div className={styles.useCaseContents}>
-          <div>REmotion はオンライン上で行う活動中のコミュニケーションをサポートします．</div>
           <div className={styles.useCaseContentsCard}>
             <h2>オンライン会議</h2>
-            <div>感情を可視化することで，カメラをオフにして参加するオンライン会議をサポートします。</div>
+            <div className={styles.imageWrapper}>
+              <Image src={MeetingImg.src} width='200px' height='200px' alt='meeting-img'></Image>
+            </div>
+            {/* <p>感情を可視化することで，カメラをオフにして参加するオンライン会議をサポートします。</p> */}
           </div>
           <div className={styles.useCaseContentsCard}>
             <h2>オンライン授業</h2>
-            <div>感情を可視化することで，カメラをオフにして参加するオンライン授業をサポートします。</div>
+            <div className={styles.imageWrapper}>
+              <Image src={ClassImg.src} width='200px' height='200px' alt='class-img'></Image>
+            </div>
+            {/* <p>感情を可視化することで，カメラをオフにして参加するオンライン授業をサポートします。</p> */}
           </div>
           <div className={styles.useCaseContentsCard}>
             <h2>WEBセミナー</h2>
-            <div>感情を可視化することで，カメラをオフにして参加するWEBセミナーをサポートします。</div>
+            <div className={styles.imageWrapper}>
+              <Image src={SeminarImg.src} width='200px' height='200px' alt='seminar-img'></Image>
+            </div>
+            {/* <p>感情を可視化することで，カメラをオフにして参加するWEBセミナーをサポートします。</p> */}
           </div>
         </div>
       </div>
