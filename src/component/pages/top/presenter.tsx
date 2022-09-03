9409d7c0ce0ac09008c686517b14dd0e2de732a6
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-09-03 18:01:29 +0900

    news area のUI作成

--- src/component/pages/top/presenter.tsx
+++ src/component/pages/top/presenter.tsx
@@ -20,9 +20,20 @@ export const TopPagePresenter: React.FC = () => {
       <UseCaseArea />
       <ArchitectureArea />
       <div className={styles.news}>
+        <h1>NEWS</h1>
         <div className={styles.newsList}>
-          <h1>NEWS</h1>
-          <div className={styles.newsListContents}></div>
+          <div className={styles.newsListContents}>
+            <p>日付 2022/8/22</p>
+            <a href='https://note.com/eggnao/n/nef95fe779cc4'>
+              <h2>note(第二話) を執筆しました</h2>
+            </a>
+          </div>
+          <div className={styles.newsListContents}>
+            <p>日付 2022/6/27</p>
+            <a href='https://note.com/eggnao/n/n77bbbf20d073'>
+              <h2>note を執筆しました</h2>
+            </a>
+          </div>
         </div>
       </div>
       <div className={styles.footer}></div>
