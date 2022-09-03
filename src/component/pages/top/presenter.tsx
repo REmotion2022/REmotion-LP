a079f3bd07ed502485a5d1111aa9f287301701fa
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-09-03 15:03:22 +0900

    Feature Area の UI 作成

--- src/component/pages/top/presenter.tsx
+++ src/component/pages/top/presenter.tsx
@@ -1,9 +1,9 @@
 import { Header } from '~/component/layouts/Header'
 import styles from './style.module.scss'
-import { Button } from '~/component/uiParts/Button'
 import { ProblemArea } from '~/component/uiParts/ProblemArea'
-import Image from 'next/image'
 import { SolutionArea } from '~/component/uiParts/SolutionArea'
+import { FeatureArea } from '~/component/uiParts/FeatureArea'
+// import Image from 'next/image'
 
 export const TopPagePresenter: React.FC = () => {
   return (
@@ -15,29 +15,11 @@ export const TopPagePresenter: React.FC = () => {
       </div>
       <ProblemArea />
       <SolutionArea />
-      <div className={styles.feature}>
-        <h1>アプリの特徴</h1>
-        <div className={styles.featureContents}>
-          <div className={styles.featureContentsCard}>
-            <h2>1.既存のオンライン会議アプリと併用できる！</h2>
-          </div>
-          <div className={styles.featureContentsCard}>
-            <h2>2.リアルタイムで参加者の表情を可視化！</h2>
-          </div>
-          <div className={styles.featureContentsCard}>
-            <h2>3.感情の時系列データをグラフで表示！</h2>
-          </div>
-          <a href='https://remotion-b2f4b.web.app/'>
-            <Button type='secondary'>使ってみる</Button>
-          </a>
-        </div>
-      </div>
+      <FeatureArea />
       <div className={styles.useCase}>
+        <h1>REmotion の使用例</h1>
         <div className={styles.useCaseContents}>
-          <div>
-            REmotion は Zoom や Google Meet， Microsoft Teams
-            など既存のオンライン会議アプリと併用することができ，オンライン上で行う活動中のコミュニケーションをサポートします．
-          </div>
+          <div>REmotion はオンライン上で行う活動中のコミュニケーションをサポートします．</div>
           <div className={styles.useCaseContentsCard}>
             <h2>オンライン会議</h2>
             <div>感情を可視化することで，カメラをオフにして参加するオンライン会議をサポートします。</div>
---
+++ src/component/uiParts/FeatureArea/index.tsx
@@ -0,0 +1,50 @@
+import styles from './style.module.scss'
+import Image from 'next/image'
+import graphImg from 'public/graph.jpeg'
+import { Button } from '../Button'
+
+export const FeatureArea: React.FC = () => {
+  return (
+    <div className={styles.feature}>
+      <h1>アプリの特徴</h1>
+      <div className={styles.featureContents}>
+        <div className={styles.featureContentsCard}>
+          <div className={styles.description}>
+            <h2>1.既存のオンライン会議アプリと併用できる！</h2>
+            <p>
+              REmotion のアプリ自体にはオンライン会議機能はありません．Zoom や Google Meet, Microsoft Teams
+              など既存のオンライン会議アプリと併用することができます．
+            </p>
+          </div>
+          <div className={styles.imageWrapper}>
+            <Image src={graphImg.src} alt='graph-img' width='900px' height='600px' />
+          </div>
+        </div>
+        <div className={`${styles.featureContentsCard} ${styles.two}`}>
+          <div className={styles.imageWrapper}>
+            <Image src={graphImg.src} alt='graph-img' width='900px' height='600px' />
+          </div>
+          <div className={styles.description}>
+            <h2>2.リアルタイムで参加者の表情を可視化！</h2>
+            <p>参加者を表す絵文字がリアルタイムで変化するため，リモートにいる相手の存在を確認することができます．</p>
+          </div>
+        </div>
+        <div className={styles.featureContentsCard}>
+          <div className={styles.description}>
+            <h2>3.参加者の感情の変化をグラフで表示！</h2>
+            <p>
+              会議中に参加者の感情がどのように変化したかをリアルタイムで変化するグラフで確認することができます．
+              <br /> また，会議後にグラフを確認することで会議を振り返ることもできます．
+            </p>
+          </div>
+          <div className={styles.imageWrapper}>
+            <Image src={graphImg.src} alt='graph-img' width='900px' height='600px' />
+          </div>
+        </div>
+        <a href='https://remotion-b2f4b.web.app/'>
+          <Button type='secondary'>使ってみる</Button>
+        </a>
+      </div>
+    </div>
+  )
+}
